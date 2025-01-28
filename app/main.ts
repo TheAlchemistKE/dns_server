f1842f20e6174b72a01d8944c593958c9ccf95d7
Kelyn Njeri <kelynpaul20.kp@gmail.com>
2025-01-28 20:02:39 +0300

    add dns messages

--- app/main.ts
+++ app/main.ts
@@ -1,17 +1,21 @@
 import * as dgram from "dgram";
+import { DNSMessageHeader } from './dnsMessage';
+
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
-// Uncomment this block to pass the first stage
+const defaultHeader = new DNSMessageHeader();
+defaultHeader.packetID = 1234;
+defaultHeader.isResponse = true;
 
 const udpSocket: dgram.Socket = dgram.createSocket("udp4");
 udpSocket.bind(2053, "127.0.0.1");
 
-udpSocket.on("message", (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+udpSocket.on('message', (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
     try {
         console.log(`Received data from ${remoteAddr.address}:${remoteAddr.port}`);
-        const response = Buffer.from("");
+        const response = Buffer.from(defaultHeader.encode());
         udpSocket.send(response, remoteAddr.port, remoteAddr.address);
     } catch (e) {
         console.log(`Error sending data: ${e}`);
